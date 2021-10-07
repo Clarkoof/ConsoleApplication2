e71d53f35bcfd3d36e758a1d09efcfa0d2bdd677
Stoyanov Andrey <clarkoof@mail.ru>
2021-10-07 19:45:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <stdint.h>
+#include <iomanip>
+int main()
+{
+	
+	std::string Letters = "sdfsdgffgf";
+	std::cout << Letters << "\n";
+	std::cout << Letters.length() << "\n";
+	char last = Letters[Letters.length() - 1];
+	std::cout << last << "\n";
+	char first = Letters[0];
+	std::cout << first;
+
+	std::cin;
+	return 0;
+}
